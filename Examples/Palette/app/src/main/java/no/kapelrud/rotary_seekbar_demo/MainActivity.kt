bcbb57e5ac2c9ee4669ff98a10006e86364abbf9
André Kapelrud <a.kapelrud@gmail.com>
2020-02-07 21:52:43 +0100

    example with 4x4 grid of RotarySeekbars
    Closes #12

---
+++ Examples/Palette/app/src/main/java/no/kapelrud/rotary_seekbar_demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package no.kapelrud.rotary_seekbar_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
