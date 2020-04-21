aa25952906e2718842c7eb025b5ace829d84c658
André Kapelrud <a.kapelrud@gmail.com>
2020-04-21 10:43:53 +0200

    Example layout change, and using setValue

--- Examples/Palette/app/src/main/java/no/kapelrud/rotary_seekbar_demo/MainActivity.kt
+++ Examples/Palette/app/src/main/java/no/kapelrud/rotary_seekbar_demo/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       
+        /* It is possible to alter the values programmatically. The value will snap to valid
+         * steps and bounds.
+         */
+        //rot01.value = 100.22f;
+        //rot02.value = -12319.0f;
+        //rot22.value = 47.5f;
     }
 }
